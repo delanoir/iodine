eb7d7b89c7ab5de2483f34b4f9682267e27eb224
Karolis Stasaitis <stkarolis@gmail.com>
2014-09-25 11:21:16 +0300

    Initial Commit

---
+++ iodine/iodine.h
@@ -0,0 +1,19 @@
+//
+//  iodine.h
+//  iodine
+//
+//  Created by Karolis Stasaitis on 25/09/14.
+//  Copyright (c) 2014 delanoir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iodine.
+FOUNDATION_EXPORT double iodineVersionNumber;
+
+//! Project version string for iodine.
+FOUNDATION_EXPORT const unsigned char iodineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iodine/PublicHeader.h>
+
+
